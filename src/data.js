6243c122330248be1ea7d591d733b4b57bcb5b33
shwethaj1104 <shwethaj1104@gmail.com>
2022-08-02 09:14:16 +0530

    styling

--- src/data.js
+++ src/data.js
@@ -1,39 +1,48 @@
 const reviews = [
   {
     id: 1,
-    name: 'susan smith',
+    name: 'Ana thomas',
     job: 'web developer',
     image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+      'https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8NDF8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
     text:
-      "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+      "This is a hipster place to enjoy a cup of freshly brewed coffee brewed by an award winning baristar. The seating arrangement here comprise of one long table to seat about 21 persons or thereabouts. Every customer sits at a single long table to enjoy the coffeee and the food served here. Ambience is nice. Decor is interesting and very hipster like. Simple food is offered here. Nothing very much though. However, the main attraction here is the freshly brewed coffee.",
   },
   {
     id: 2,
-    name: 'anna johnson',
+    name: 'Levi smith',
     job: 'web designer',
     image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg',
+      'https://images.unsplash.com/photo-1524255684952-d7185b509571?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8NjV8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
     text:
-      'Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.',
+      'The barista here has won the champion Malaysia barista award for the years 2016 and 2018. Needless to say, the coffee served here is good with the right temperature. The coffee served has a slight sour taste to the taste buds, not too sour and is certainly not bitter. It is a chic place to drop by to enjoy a cup of coffee after a meal at Village Park restaurant which is this cafeteria is immediate neighbour. The place is very small though.',
   },
   {
     id: 3,
-    name: 'peter jones',
-    job: 'intern',
+    name: 'Noah',
+    job: 'Book seller',
     image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
+      'https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8N3x8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60',
     text:
-      'Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.',
+      'There are not enough food, service, value or atmosphere ratings for Random Food Store, Malaysia yet. Be one of the first to write a review!',
   },
   {
     id: 4,
-    name: 'bill anderson',
-    job: 'the boss',
+    name: 'Amy Singh',
+    job: 'Teacher',
     image:
-      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
+      'https://images.unsplash.com/photo-1525134479668-1bee5c7c6845?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8ODZ8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
     text:
-      'Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ',
+      'Visited this during my short getaway to PJ on last weekend. Hipster, futuristic & simple design of the cafe caught all my attention. One long table, small space, but so enjoy sitting and taking the coffee. The coffee is most cafes more or less the similar (maybe I can not differentiate), and theirs definitely different. I can not tell if I like it, but it goes well in the cup and mouth. Food maybe good to some others but not my type. ',
+  },
+  {
+    id: 5,
+    name: 'Daniel anderson',
+    job: 'Doctor',
+    image:
+      'https://images.unsplash.com/photo-1528892952291-009c663ce843?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mjl8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+    text:
+      'A simple interior with a long stone-top table. I like it’s simplicity. However the coffee served is not simple at all. The wonderful barista explains the way to best enjoy the super ice latte. The fresh milk used isn’t simply poured from the carton. They “removed” water from the milk to leave the creamy milk for the exquisite taste of cafe latte! Brilliant! Another to die for is the TKG, it’s rice with truffle oil, caviar and raw egg. Love every taste bursting in my mouth. It is next to Village Park Nasi Lemak. Go for it!!!',
   },
 ];
 
